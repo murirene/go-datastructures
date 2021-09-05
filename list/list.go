80145bfc6328b9e40d96411edeabeda808f90e9c
Rene <rene.murillo@ymail.com>
2021-09-05 10:29:27 -0700

    Added the Remove From the Head and fixed AddFirst

--- list/list.go
+++ list/list.go
@@ -1,6 +1,8 @@
 // Package list has methods to make and manage linked lists
 package list
 
+import "errors"
+
 type node struct {
 	value int
 	next  *node
@@ -25,20 +27,49 @@ func MakeLinkedList() LinkedList {
 
 // Add to the head of the linked list
 func (list *LinkedList) AddFirst(value int) {
-	if list.head == nil {
-		list.head = &node{
-			value: value,
-			next:  nil,
-		}
-		list.tail = list.head
-		list.size = 0
+    newNode := &node{
+		value: value,
+		next:  nil,
 	}
+
+	if list.head == nil {
+		list.head = newNode
+		list.tail = newNode
+		list.size = 1
+		return
+	}
+
+    tmp := list.head
+	list.head = newNode
+	list.head.next = tmp
+	list.size += 1
 }
 
-func (list LinkedList) GetSize() (int, bool) {
-	return list.size, list.head == nil
+// Remove from the Head
+func (list *LinkedList) RemoveFirst() (int, error) {
+    if list.head == nil {
+        return 0, errors.New("Can't remove from an Empty List")
+    }
+    
+    purged := list.head
+    list.head = list.head.next
+    list.size -= 1
+    if list.size == 0 {
+        list.tail = nil
+    }
+
+    return purged.value, nil
 }
 
-func (list LinkedList) HeadPeek() (int, bool) {
+// Get the Linked List Size
+func (list LinkedList) GetSize() int {
+    if list.head == nil {
+        return 0
+    }
+
+	return list.size
+}
+
+func (list LinkedList) Peek() (int, bool) {
 	return list.size, list.head == nil
 }
--- list_test/list_test.go
+++ list_test/list_test.go
@@ -7,23 +7,62 @@ import (
 
 func CreateUpdateLinkedListTest(t *testing.T) {
 	linklist := list.MakeLinkedList()
-	size, isEmpty := linklist.GetSize()
+	size := linklist.GetSize()
 
-	if size != 0 && !isEmpty {
+	if size != 0 { 
 		t.Fatal("New LinkedList size is not 0 and is not empty")
 	}
 
 	linklist.AddFirst(9)
 	linklist.AddFirst(7)
 	linklist.AddFirst(10)
-
-	size, isEmpty = linklist.GetSize()
+	size = linklist.GetSize()
 
 	if size != 3 {
 		t.Fatalf("Linklist Size %d is not 3", size)
 	}
 
-	if isEmpty == true {
-		t.Fatalf("Linked List of size %d is returning Empty Status", size)
+    removed, err := linklist.RemoveFirst()
+    if removed != 10 {
+        t.Fatalf("Removed First %d", removed)
+    }
+
+    peeked, isEmpty := linklist.Peek()
+    if peeked != 7 && isEmpty == false {
+        t.Fatalf("Failed Peek returned %d", peeked)
+    }
+
+    removed, err = linklist.RemoveFirst()
+    if removed != 7 {
+        t.Fatalf("Removed First %d", removed)
+    }
+
+	size = linklist.GetSize()
+
+	if size != 1 {
+		t.Fatalf("Linklist Size %d is not 3", size)
+	}
+
+    removed, err = linklist.RemoveFirst()
+    
+    if removed != 9 {
+        t.Fatalf("Removed First %d", removed)
+    }
+    
+	size = linklist.GetSize()
+
+	if size != 0 {
+		t.Fatalf("Linklist Size %d is not 3", size)
+	}
+
+    removed, err = linklist.RemoveFirst()
+    if err == nil  {
+        t.Fatalf("Removing First on Empty Linked List did not return ")
+    }
+
+	size = linklist.GetSize()
+
+	if size != 0 {
+		t.Fatalf("Linklist Size %d is not 3", size)
 	}
 }
